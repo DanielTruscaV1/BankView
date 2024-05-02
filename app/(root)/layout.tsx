b9568705e130bcd3fe923d4bddf336c05b608e22
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-03 01:24:57 +0300

    Update V3

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,19 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { 
+    firstName: 'Daniel',
+    lastName: 'Trusca',
+  }
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar/>
         {children}
     </main>
   );
