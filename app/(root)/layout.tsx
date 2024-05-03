77e100e0785f08fe8dc7c5b7f8782a68fa30704a
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-03 13:41:50 +0300

    Update V5.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -13,8 +16,22 @@ export default function RootLayout({
 
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar/>
-        {children}
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image
+              src="/icons/logo.svg"
+              width={30}
+              height={30}
+              alt="menu icon"
+            />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
